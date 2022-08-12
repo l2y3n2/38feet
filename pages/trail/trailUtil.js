7de11ba268ab13f69fbfde1f51db89e4cc0c31c5
Lin Yuning <l2y3n2@gmail.com>
2022-08-12 17:18:00 +0800

    bug fix

--- pages/trail/trailUtil.js
+++ pages/trail/trailUtil.js
@@ -198,7 +198,8 @@ function addPoint(page, point) {
       totalTime: Math.round((now - page.data.startTime) / 1000),
       totalDistance: Math.round(totalDistance),
       avgSpeed: Math.round(totalDistance / (now - page.data.startTime) * 1000),
-      maxSpeed: Math.round(page.data.maxSpeed)
+      maxSpeed: Math.round(page.data.maxSpeed),
+      counter: page.data.counter + 1
     });
   }
   else {
